53e2cd3e553a1fb0b09604368ad8b2f93e58170f
Mohamed Shehab <mohamed.shehab@rocket-internet.de>
2014-06-20 03:30:45 +0200

    Making the structure easier

--- src/App.php
+++ src/App.php
@@ -11,38 +11,52 @@ class App
     {
         $this->di = new \Aura\Di\Container(new \Aura\Di\Forge(new \Aura\Di\Config()));
         $this->init();
+        $this->initParams();
         $this->initCrawler();
     }
 
     protected function init()
     {
         $di = $this->getDi();
-        $di->set("factory", $di->lazyNew('\Aura\Http\Message\Factory'));
-        $di->set("options", $di->lazyNew('\Aura\Http\Transport\Options'));
-        $di->set("phpFunc", $di->lazyNew('\Aura\Http\PhpFunc'));
-        $di->set("stack",$di->lazyNew('\Aura\Http\Message\Response\StackBuilder',
-                ['message_factory' => $di->lazyGet('factory')]
-            )
-        );
-        $di->set("transport", $di->lazyNew('\Aura\Http\Transport', [
-                'adapter' => $di->lazyGet("curl"),
-                'options' => $di->lazyGet("options"),
-                'phpfunc' => $di->lazyGet("phpFunc")
-            ]
-        ));
-        $di->set("httpClient", $di->lazyNew('\Aura\Http\Manager',
-                ['message_factory' => $di->lazyGet('factory'), 'transport' => $di->lazyGet('transport')]
-            )
-        );
-        $di->set("curl", $di->lazyNew('\Aura\Http\Adapter\Curl', ['stack_builder' => $di->lazyGet('stack')]));
+        $di->set('phpFunc',     $di->lazyNew('\Aura\Http\PhpFunc'));
+        $di->set('httpClient',  $di->lazyNew('\Aura\Http\Manager'));
+        $di->set('transport',   $di->lazyNew('\Aura\Http\Transport'));
+        $di->set('curl',        $di->lazyNew('\Aura\Http\Adapter\Curl'));
+        $di->set('factory',     $di->lazyNew('\Aura\Http\Message\Factory'));
+        $di->set('options',     $di->lazyNew('\Aura\Http\Transport\Options'));
+        $di->set('stack',       $di->lazyNew('\Aura\Http\Message\Response\StackBuilder'));
+    }
+
+    protected function initParams()
+    {
+        $di = $this->getDi();
+
+        $di->parameters['\Aura\Http\Message\Response\StackBuilder'] = [
+            'message_factory'   => $di->lazyGet('factory')
+        ];
+
+        $di->parameters['\Aura\Http\Adapter\Curl'] = [
+            'stack_builder'     => $di->lazyGet('stack')
+        ];
+
+        $di->parameters['\Aura\Http\Manager'] = [
+            'message_factory'   => $di->lazyGet('factory'),
+            'transport'         => $di->lazyGet('transport')
+        ];
+
+        $di->parameters['\Aura\Http\Transport'] = [
+            'adapter'           => $di->lazyGet('curl'),
+            'options'           => $di->lazyGet('options'),
+            'phpfunc'           => $di->lazyGet('phpFunc')
+        ];
     }
 
     protected function initCrawler()
     {
-        $this->getDi()->params['\Http\Crawler'] = array(
+        $this->getDi()->params['\Http\Crawler'] = [
             'password' => 'injectedPassword',
-            'httpClient' => $this->getDi()->lazyGet('httpClient'),
-        );
+            'httpClient' => $this->getDi()->lazyGet('httpClient')
+        ];
         $this->getDi()->set('crawler', $this->getDi()->lazyNew('\Http\Crawler'));
     }
 
@@ -60,7 +74,7 @@ class App
      */
     public function getHttpClient()
     {
-        return $this->getDi()->get("httpClient");
+        return $this->getDi()->get('httpClient');
     }
 
     /**
@@ -69,7 +83,7 @@ class App
      */
     public function getCrawler()
     {
-        return $this->getDi()->get("crawler");
+        return $this->getDi()->get('crawler');
     }
 
     /**
@@ -79,7 +93,7 @@ class App
     public function getSpecialCrawler($mixed)
     {
         if (!is_array($mixed)) {
-            $mixed = array("username" => $mixed);
+            $mixed = ['username' => $mixed];
         }
         $params = array_merge(
             $this->getDi()->params['\Http\Crawler'],
